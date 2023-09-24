1e757bfc18ff0c7749fddbe7c712d441e0684f9c
JheronVallangca <145763056+JheronVallangca@users.noreply.github.com>
2023-09-24 15:55:25 +0800

    Update BakeACake.kt

--- BakeACake.kt
+++ BakeACake.kt
@@ -1,44 +1,49 @@
-fun main() {
-    val CakeCandles = ","
-    val CakeCandlesOne = "|"
-    val CakeTop = "="
-    val CakeBottom = "@"
+fun main() 
+{
     val Age = 20
     val layer = 5
     
-    printBorder(CakeCandles,Age)
-    printBorderOne(CakeCandlesOne,Age)
-    printBorderTwo(CakeTop,Age)
-    printBorderThree(CakeBottom,layer,Age)
-
-   
+    printCakeCandles(Age)
+    printCakeCandlesBody(Age)
+    printCakeTop(Age)
+    printCakeBottom(layer,Age)
 }
-fun printBorder(CakeCandles:String, Age:Int){
-    repeat(Age){
-    print(CakeCandles)
+fun printCakeCandles(Age:Int)
+{
+    repeat(Age)
+    {
+        print(",")
     }
     println()
 }
-
-fun printBorderOne(CakeCandlesOne:String, Age:Int){
-    repeat(Age){
-    print(CakeCandlesOne)
+​
+fun printCakeCandlesBody(Age:Int)
+{
+    repeat(Age)
+    {
+        print("|")
     }
     println()
 }
-
-fun printBorderTwo(CakeTop:String, Age:Int){
-    repeat(Age){
-    print(CakeTop)
+​
+fun printCakeTop(Age:Int)
+{
+    repeat(Age)
+    {
+        print("=")
     }
     println()
 }
-
-fun printBorderThree(CakeBottom:String, layer:Int ,Age:Int){
-    repeat(layer){
-        repeat(Age){
-    print(CakeBottom)
+​
+fun printCakeBottom(layer:Int ,Age:Int)
+{
+    repeat(layer)
+    {
+        repeat(Age)
+        {
+            print("@")
         }
         println()
 }
 }
+​
